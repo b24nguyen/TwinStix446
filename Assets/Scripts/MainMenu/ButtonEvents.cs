e6938a8352d7eee5fd94d5916e11a55f2f294fef
Matthew Pitropov <matthewpitropov@gmail.com>
2017-01-31 18:15:41 -0500

    Added main menu, the ability to click play to load another scene and created folders to structure the project.

---
+++ Assets/Scripts/MainMenu/ButtonEvents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonEvents : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClickPlay()
+    {
+        SceneManager.LoadScene("test");
+    }
+}
