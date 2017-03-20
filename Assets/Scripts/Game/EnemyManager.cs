76af8d3a84a51cb6799ae939d1022192d48bb87d
ATN Long <long.etienne@gmail.com>
2017-03-20 15:01:40 -0400

    Added spawning system for ranged enemies.

--- Assets/Scripts/Game/EnemyManager.cs
+++ Assets/Scripts/Game/EnemyManager.cs
@@ -2,26 +2,42 @@
 
 public class EnemyManager : MonoBehaviour
 {
-    public GameObject enemy;                // The enemy prefab to be spawned.
-    public float spawnTime;                 // How long between each spawn.
-    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    public GameObject basicEnemy;           // The basic enemy prefab to be spawned.
+    public GameObject rangedEnemy;          // The ranged enemy prefab to be spawned.
+    public float spawnTimeBasicEnemy;       // How long between each basic enemy spawn.
+    public float spawnTimeRangedEnemy;      // How long between each ranged enemy spawn.
+    public Transform[] spawnPointsBasic;    // An array of the spawn points basic enemies can spawn from.
+    public Transform[] spawnPointsRanged;   // An array of the spawn points basic enemies can spawn from.
     private ItemManager itemManager;        // Item Manager to pass to enemies
 
     void Start()
     {
         itemManager = GameObject.Find("ItemManager").GetComponent<ItemManager>();
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        InvokeRepeating("SpawnBasicEnemy", spawnTimeBasicEnemy, spawnTimeBasicEnemy);
+        InvokeRepeating("SpawnRangedEnemy", spawnTimeRangedEnemy, spawnTimeRangedEnemy);
     }
 
 
-    void Spawn()
+    void SpawnBasicEnemy()
     {
         // Find a random index between zero and one less than the number of spawn points.
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int spawnPointIndex = Random.Range(0, spawnPointsBasic.Length);
 
         // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        GameObject newEnemy = Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation) as GameObject;
+        GameObject newEnemy = Instantiate(basicEnemy, spawnPointsBasic[spawnPointIndex].position, spawnPointsBasic[spawnPointIndex].rotation) as GameObject;
+        // Send over reference of the item drops manager to the enemy
+        EnemyHealthManager enemyHealthManager = newEnemy.GetComponent<EnemyHealthManager>();
+        enemyHealthManager.initGameObjects(itemManager);
+    }
+
+    void SpawnRangedEnemy()
+    {
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPointsRanged.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        GameObject newEnemy = Instantiate(rangedEnemy, spawnPointsRanged[spawnPointIndex].position, spawnPointsRanged[spawnPointIndex].rotation) as GameObject;
         // Send over reference of the item drops manager to the enemy
         EnemyHealthManager enemyHealthManager = newEnemy.GetComponent<EnemyHealthManager>();
         enemyHealthManager.initGameObjects(itemManager);
